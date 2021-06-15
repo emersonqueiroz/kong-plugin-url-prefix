570160f270331971b45b448af6c0eae2a9deaad6
Emerson Queiroz <equeiroz.sp@gmail.com>
2021-06-15 11:07:34 -0300

    Moving logic for single file and fixing plugin version at handler

--- kong/plugins/url-prefix/handler.lua
+++ kong/plugins/url-prefix/handler.lua
@@ -1,11 +1,23 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local access = require "kong.plugins.url-prefix.access"
-
+local get_path = kong.request.get_path
+local set_path = kong.service.request.set_path
 
 local UrlPrefix = BasePlugin:extend()
 
-UrlPrefix.VERSION  = "1.0.0"
+UrlPrefix.VERSION = "0.1.1"
 UrlPrefix.PRIORITY = 810
+
+local function is_empty(s)
+    return s == nil or s == ''
+end
+
+local function ltrim(str, trim_str)
+    if string.find(str, trim_str) == 1 then
+        return str:sub(string.len(trim_str) + 1)
+    else
+        return str
+    end
+end
 
 function UrlPrefix:new()
     UrlPrefix.super.new(self, "url-prefix")
@@ -13,7 +25,9 @@ end
 
 function UrlPrefix:access(config)
     UrlPrefix.super.access(self)
-    access.execute(config)
+    if not is_empty(config.prefix) then
+        set_path(ltrim(get_path(), config.prefix))
+    end
 end
 
 return UrlPrefix
